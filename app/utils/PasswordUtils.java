3be03b6aab2664d8d73e36cf88cb7a0d146f37ae
Jostein Tveit <josteitv@pvv.ntnu.no>
2013-08-18 19:24:42 +0200

    Removed main method in PasswordUtils

--- app/utils/PasswordUtils.java
+++ app/utils/PasswordUtils.java
@@ -4,12 +4,6 @@ import org.jasypt.util.password.StrongPasswordEncryptor;
 
 public class PasswordUtils {
 
-    public static void main(String[] args) {
-        StrongPasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();
-        String hashed = passwordEncryptor.encryptPassword("secret");
-        System.out.println(hashed);
-    }
-
     public static String encryptPassword(String plainPassword) {
         StrongPasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();
         return passwordEncryptor.encryptPassword(plainPassword);
